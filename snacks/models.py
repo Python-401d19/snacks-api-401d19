3cf293e54900be6124529b549bb60b88db316429
JB-Tellez <jb.tellez@gmail.com>
2022-09-01 11:38:25 -0700

    converts to snacks

--- snacks/models.py
+++ snacks/models.py
@@ -2,12 +2,12 @@ from django.contrib.auth import get_user_model
 from django.db import models
 
 
-class Thing(models.Model):
+class Snack(models.Model):
     name = models.CharField(max_length=256)
-    owner = models.ForeignKey(
+    purchaser = models.ForeignKey(
         get_user_model(), on_delete=models.CASCADE, null=True, blank=True
     )
-    description = models.TextField(default="", null=True, blank=True)
+    rating = models.IntegerField(default=0)
 
     def __str__(self):
         return self.name
